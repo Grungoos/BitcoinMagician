e576453011c3e6bb811bbeb1ea7bcbc95829972a
Grungoos <Volkerjovanovic@outlook.de>
2023-11-06 03:45:23 +0100

    Neural-network-past-prediction

--- main.py
+++ main.py
@@ -2,54 +2,66 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 import numpy as np
 import yfinance as yf
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
+from keras.layers import Dense, LSTM
+from keras.models import Sequential
+from sklearn.preprocessing import MinMaxScaler
 
 # Daten für Bitcoin in Euro herunterladen
-data = yf.download('BTC-EUR', start='2016-01-01', end=datetime.today().strftime('%Y-%m-%d'))
-
-# Aktueller Preis
-current_price = data['Close'][-1]
+data = yf.download('BTC-EUR', start='2021-01-01', end=datetime.today().strftime('%Y-%m-%d'))
 
 # 'Close' Preis als Zielvariable verwenden
-data['Prediction'] = data['Close'].shift(-1)
+data = data[['Close']]
 
-# Die letzten 'n' Zeilen entfernen
-n = 30  # Anzahl der Tage, die wir vorhersagen möchten
-data = data[:-n]
+# Daten normalisieren
+scaler = MinMaxScaler(feature_range=(0,1))
+scaled_data = scaler.fit_transform(data)
 
-# Unabhängige Datensatz erstellen
-X = np.array(data.drop(['Prediction'], axis=1))
+# Trainingsdatensatz erstellen
+train_data = scaled_data[0:int(len(scaled_data)*0.8), :]
 
-# Zielvariable erstellen
-y = np.array(data['Prediction'])
+# Trainingsdaten in x_train und y_train aufteilen
+x_train, y_train = [], []
+for i in range(60, len(train_data)):
+    x_train.append(train_data[i-60:i, 0])
+    y_train.append(train_data[i, 0])
+x_train, y_train = np.array(x_train), np.array(y_train)
 
-# Daten in Trainings- und Testdaten aufteilen
-x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+# Daten für LSTM-Modell umformen (samples, time steps, features)
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
 
-# Lineares Regressionsmodell erstellen
-lr = LinearRegression()
+# LSTM-Modell erstellen
+model = Sequential()
+model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1], 1)))
+model.add(LSTM(units=50, return_sequences=False))
+model.add(Dense(units=25))
+model.add(Dense(units=1))
 
-# Modell trainieren
-lr.fit(x_train, y_train)
+# Modell kompilieren und trainieren
+model.compile(optimizer='adam', loss='mean_squared_error')
+model.fit(x_train, y_train, batch_size=1, epochs=1)
 
-# Modell testen
-lr_confidence = lr.score(x_test, y_test)
-print("lr confidence: ", lr_confidence)
+# Testdatensatz erstellen
+test_data = scaled_data[int(len(scaled_data)*0.8) - 60:, :]
 
-# 'n' Zeilen des originalen Datensatzes anzeigen
-x_forecast = np.array(data.drop(['Prediction'], axis=1))[-n:]
+# Testdaten in x_test und y_test aufteilen
+x_test, y_test = [], data[int(len(scaled_data)*0.8):]
+for i in range(60, len(test_data)):
+    x_test.append(test_data[i-60:i, 0])
+x_test = np.array(x_test)
 
-# Vorhersage für die nächsten 'n' Tage
-lr_prediction = lr.predict(x_forecast)
-print(lr_prediction)
+# Daten für LSTM-Modell umformen
+x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+
+# Vorhersagen machen
+predictions = model.predict(x_test)
+predictions = scaler.inverse_transform(predictions)
 
 # Diagramm erstellen
 plt.figure(figsize=(12,6))
-plt.plot(range(len(lr_prediction)), lr_prediction, color='blue', linestyle='-', label='Predicted Price')
-plt.scatter(0, current_price, color='red', label='Current Price')  # Aktuellen Preis anzeigen
-plt.title('Bitcoin price prediction for next 30 days')
-plt.xlabel('Days')
-plt.ylabel('Price (€)')
+plt.plot(y_test.values, color='blue', label='Actual Bitcoin Price')
+plt.plot(predictions , color='red', label='Predicted Bitcoin Price')
+plt.title('Bitcoin Price Prediction')
+plt.xlabel('Date')
+plt.ylabel('Price')
 plt.legend()
 plt.show()
