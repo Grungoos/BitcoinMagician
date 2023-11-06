05270cab07c8a7c45ec66b618e5d5c05c620ff39
Grungoos <Volkerjovanovic@outlook.de>
2023-11-06 04:26:49 +0100

    Neural-network-future-prediction

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-from datetime import datetime
-import matplotlib.pyplot as plt
 import numpy as np
-import yfinance as yf
-from keras.layers import Dense, LSTM
-from keras.models import Sequential
+import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers import Dense, LSTM
+import yfinance as yf
+from datetime import datetime
 
 # Daten für Bitcoin in Euro herunterladen
-data = yf.download('BTC-EUR', start='2021-01-01', end=datetime.today().strftime('%Y-%m-%d'))
+data = yf.download('BTC-EUR', start='2014-01-01', end=datetime.today().strftime('%Y-%m-%d'))
 
 # 'Close' Preis als Zielvariable verwenden
 data = data[['Close']]
@@ -17,7 +18,7 @@ scaler = MinMaxScaler(feature_range=(0,1))
 scaled_data = scaler.fit_transform(data)
 
 # Trainingsdatensatz erstellen
-train_data = scaled_data[0:int(len(scaled_data)*0.8), :]
+train_data = scaled_data
 
 # Trainingsdaten in x_train und y_train aufteilen
 x_train, y_train = [], []
@@ -38,30 +39,26 @@ model.add(Dense(units=1))
 
 # Modell kompilieren und trainieren
 model.compile(optimizer='adam', loss='mean_squared_error')
-model.fit(x_train, y_train, batch_size=1, epochs=1)
+model.fit(x_train, y_train, batch_size=1, epochs=50)  # Anzahl der Epochen auf 50 erhöht
 
-# Testdatensatz erstellen
-test_data = scaled_data[int(len(scaled_data)*0.8) - 60:, :]
+# Vorhersage für die nächsten 'n' Tage
+n = 30
+predictions = []
+for _ in range(n):
+    x_input = train_data[-60:]
+    x_input = x_input.reshape((1, -1, 1))
+    y_hat = model.predict(x_input)
+    predictions.append(y_hat[0][0])
+    train_data = np.append(train_data, y_hat)
+    train_data = train_data[-60:]
 
-# Testdaten in x_test und y_test aufteilen
-x_test, y_test = [], data[int(len(scaled_data)*0.8):]
-for i in range(60, len(test_data)):
-    x_test.append(test_data[i-60:i, 0])
-x_test = np.array(x_test)
-
-# Daten für LSTM-Modell umformen
-x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
-
-# Vorhersagen machen
-predictions = model.predict(x_test)
-predictions = scaler.inverse_transform(predictions)
+predictions = scaler.inverse_transform(np.array(predictions).reshape(-1, 1))
 
 # Diagramm erstellen
 plt.figure(figsize=(12,6))
-plt.plot(y_test.values, color='blue', label='Actual Bitcoin Price')
-plt.plot(predictions , color='red', label='Predicted Bitcoin Price')
-plt.title('Bitcoin Price Prediction')
-plt.xlabel('Date')
+plt.plot(range(len(predictions)), predictions, color='blue', label='Predicted Bitcoin Price')
+plt.title('Bitcoin Price Prediction for next 30 days')
+plt.xlabel('Days')
 plt.ylabel('Price')
 plt.legend()
 plt.show()
